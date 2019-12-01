631d998a5d39fde79c50c4f36c8b48196dc3f271
un-know-n <dobrovolskiyevgeniy2005@gmail.com>
2019-12-01 20:13:26 +0200

    New site added!

---
+++ site-2/assets/js/carousel.js
@@ -0,0 +1,15 @@
+//$('.section-items').owlCarousel({
+//    loop:true,
+//    margin:10,
+//    responsive:{
+//        500:{
+//            items:1
+//        },
+//        930:{
+//            items:2
+//        },
+//        1000:{
+//            items:3           
+//        }
+//    }
+//})
