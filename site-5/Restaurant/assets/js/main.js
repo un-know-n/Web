bb0576f6e5fafd3ed10e5c028eb9a355620725bb
un-know-n <dobrovolskiyevgeniy2005@gmail.com>
2020-03-04 19:36:30 +0200

    Just one more

---
+++ site-5/Restaurant/assets/js/main.js
@@ -0,0 +1,17 @@
+$('.comment').owlCarousel({
+   loop:true,
+   margin:10,
+   nav:false,
+   autoplay : true,
+   responsive:{
+       0:{
+           items:1
+       },
+       600:{
+           items:1
+       },
+       1000:{
+           items:1
+       }
+   }
+})
