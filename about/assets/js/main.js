77149857c201c7681724d63f96ed9711f95e8350
un-know-n <dobrovolskiyevgeniy2005@gmail.com>
2022-07-28 22:26:54 +0300

    Media queries part 1

--- about/assets/js/main.js
+++ about/assets/js/main.js
@@ -1,3 +1,32 @@
+
+preventAnchorScroll();
+/**
+ * Prevent automatic scrolling of page to anchor by browser after loading of page.
+ * Do not call this function in $(...) or $(window).on('load', ...),
+ * it should be called earlier, as soon as possible.
+ */
+ function preventAnchorScroll() {
+  var scrollToTop = function () {
+      $(window).scrollTop(0);
+  };
+  if (window.location.hash) {
+      // handler is executed at most once
+      $(window).one('scroll', scrollToTop);
+      window.location.hash = '';
+  }
+
+  // make sure to release scroll 1 second after document readiness
+  // to avoid negative UX
+  $(function () {
+      setTimeout(
+          function () {
+              $(window).off('scroll', scrollToTop);
+          },
+          1000
+      );
+  });
+}
+
 wow = new WOW(
   {
   boxClass:     'wow',     
@@ -24,14 +53,14 @@ $(document).ready(function(){
             breakpoint: 1024,
             settings: {
               slidesToShow: 3,
-              slidesToScroll: 3,
+              slidesToScroll: 1,
             }
           },
           {
-            breakpoint: 600,
+            breakpoint: 870,
             settings: {
               slidesToShow: 2,
-              slidesToScroll: 2
+              slidesToScroll: 1
             }
           },
           {
