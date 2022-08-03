6bd47cde76a33096c2258f4f68d19ee8b4ed1ee8
un-know-n <dobrovolskiyevgeniy2005@gmail.com>
2022-08-03 22:37:31 +0300

    Session one

---
+++ js-projects/assets/js/main.js
@@ -0,0 +1,128 @@
+"use strict"
+
+/*
+Session #1 
+
+literals - fixed values, constants and variables
+(ab|ba) - regular expression
+
+const smth = {
+    name: 'Nine',
+    age: 123,
+    msg: 'something...'
+}
+
+smth.age = 18;
+console.log(smth);
+
+types: undefined, null, boolean, number, BigInt, string, 
+symbol, object, function;
+
+const bigInt = 34712534752136451287345n;
+console.log(typeof bigInt);
+
+infinity(56 / 0), -infinity(-56 / 0), NaN('somehting' / 10);
+
+let userAge = 36;
+let userAgeInfo = `Age: ${userAge}`;
+console.log(userAgeInfo);
+
+let id = Symbol("id");
+console.log(typeof id);
+
+let funcVar = function name(params) {
+    //code
+}
+console.log(typeof funcVar);
+
+const userAge = 18;
+console.log(userAge);
+userAge = String(userAge);
+console.log(typeof userAge);
+
+let userAge = '72' / '2';
+console.log(typeof userAge); ----> number - 36
+
+let userAge = 30 - 6; - binary operator
+let userAge = -6; - unary operator
+
+x = 11 % 3; - rest of duplicate
+x = 5 ** 3; - power something
+
+let result = '2' + 2;
+console.log(result); ---> 22
+
+operator + can differ values
+
+let result = +'25';
+console.log(result); ---> number - 25
+
+let number = 22;
+let result = number + '';
+console.log(typeof result); ---> string
+
+let result = '25' - 5;
+console.log(result); ---> number - 20
+
+let result = 10 * '80';
+console.log(result); ---> number - 800
+
+let result = 10 * 'something';
+console.log(result); ---> NaN
+
+Special attention to the priority of the operators
+
+let counter = 3;
+counter += 2;
+counter *= 5;
+console.log(counter); ---> number - 25
+
+let usersCounter = (8 + 2, 19 + 1);
+console.log(usersCounter); ---> last expression - 20
+
+Byte operators:
+AND (&)
+OR (|)
+XOR (^)
+NOT (!)
+LEFT SHIFT (<<)
+RIGHT SHIFT (>>)
+ZERO-FILL RIGHT SHIFT (>>>)
+
+console.log('B' > 'A'); - true
+console.log('some' < 'someth'); - true
+
+console.log(null === undefined); - false
+console.log(null == undefined); - true
+
+null - 0, undefined - NaN
+
+console.log(null > 0); - false
+console.log(null == 0); - false
+console.log(null >= 0); - true
+
+Operator (||) - return first "true" or last value
+Operator (&&) - return first "false" or last value
+priority of && is higher than ||
+operator ! has the highest priority
+
+----------------------------------------------------------------
+
+operator ?? compares first value with undefined / null, and returns
+first if true, second - if false
+
+console.log(name ?? "No name"); ---> No name
+
+----------------------------------------------------------------
+
+
+
+
+Session #2
+
+
+
+
+
+*/
+
