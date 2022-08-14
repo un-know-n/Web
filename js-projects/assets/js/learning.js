a0ccad338bfe915cab70427de8e918f405a4ca7c
un-know-n <dobrovolskiyevgeniy2005@gmail.com>
2022-08-14 21:58:36 +0300

    Prepared to future projects & server experienced
    Future projects are Unsplash API & Machine typing in js.
    Also experienced a server abilities with node.js & express.js

--- js-projects/assets/js/learning.js
+++ js-projects/assets/js/learning.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 /*
 ____________Freelancer-life-style course____________
@@ -1825,6 +1825,25 @@ console.log(double(3));
 console.log(double(4));
 double - partial function from multiply
 
+_________________________EasyCode_course_________________________
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -1851,7 +1870,7 @@ double - partial function from multiply
 //         horsesPower: 120,
 //         isTurnOn:false
 //     }
-// } 
+// }
 
 // Car.prototype.start = function() {
 //     this.isTurnOn = true;
@@ -1892,14 +1911,13 @@ double - partial function from multiply
 //     for (let i = 0; i < 6; i++) {
 //         let random = Math.floor(Math.random() * hexArr.length);
 //         hexChoice += (hexArr[random]);
-        
+
 //     }
-    
+
 //     return hexChoice;
 // }
 
 //-----------------------------------------------------------
-
 
 //Calculator-------------------------------------------------
 
@@ -1941,73 +1959,80 @@ const flashcardsContainer = document.getElementById('flashcards');
 const answerTextArea = document.getElementById('answer');
 const questionTextArea = document.getElementById('question');
 
-let contentArray = localStorage.getItem('items') ? 
-JSON.parse(localStorage.getItem('items')) : []; 
+let contentArray = localStorage.getItem('items')
+  ? JSON.parse(localStorage.getItem('items'))
+  : [];
 
 contentArray.forEach(cardMaker);
 
-function hideFlashBox(){
-    createCardBlock.className = 'hide';
+function hideFlashBox() {
+  createCardBlock.className = 'hide';
 }
 
-function showFlashBox(){
-    createCardBlock.className = 'show';
+function showFlashBox() {
+  createCardBlock.className = 'show';
 }
 
-function deleteCards(){
-    localStorage.clear();
-    flashcardsContainer.innerHTML ='';
-    contentArray = [];
+function deleteCards() {
+  localStorage.clear();
+  flashcardsContainer.innerHTML = '';
+  contentArray = [];
 }
 
-function addFlashCard(){
-    let flashcardInfo = {
-        'my_question': questionTextArea.value,
-        'my_answer': answerTextArea.value
+function addFlashCard() {
+  let flashcardInfo = {
+    my_question: questionTextArea.value,
+    my_answer: answerTextArea.value,
+  };
+
+  contentArray.push(flashcardInfo);
+  localStorage.setItem('items', JSON.stringify(contentArray));
+
+  cardMaker(contentArray[contentArray.length - 1], contentArray.length - 1);
+  questionTextArea.value = '';
+  answerTextArea.value = '';
+}
+
+function cardMaker(card, currentIndex) {
+  let flashcardDiv = document.createElement('div');
+  let answer = document.createElement('h2');
+  let question = document.createElement('h2');
+  let flashcardDelete = document.createElement('i');
+
+  flashcardDiv.className = 'flashcard';
+  flashcardDelete.className = 'fa-solid fa-minus';
+
+  question.setAttribute(
+    'style',
+    'border-top:1px solid red; padding: 15px; margin-top:30px'
+  );
+  question.innerHTML = card.my_question;
+
+  answer.setAttribute(
+    'style',
+    'text-align:center; display:none; color:red; position:absolute; top:100px; left: 40%'
+  );
+  answer.innerHTML = card.my_answer;
+
+  flashcardDiv.append(answer);
+  flashcardDiv.append(question);
+  flashcardDiv.append(flashcardDelete);
+
+  flashcardDiv.addEventListener('click', function () {
+    if (answer.style.display == 'none') {
+      answer.style.display = 'block';
+    } else {
+      answer.style.display = 'none';
     }
+  });
 
-    contentArray.push(flashcardInfo);
+  flashcardDelete.addEventListener('click', function () {
+    contentArray.splice(currentIndex, 1);
     localStorage.setItem('items', JSON.stringify(contentArray));
+    flashcardDiv.remove();
+  });
 
-    cardMaker(contentArray[contentArray.length - 1], contentArray.length - 1);
-    questionTextArea.value = '';
-    answerTextArea.value = '';
-}
-
-function cardMaker(card, currentIndex){    
-    let flashcardDiv = document.createElement('div');
-    let answer = document.createElement('h2');
-    let question = document.createElement('h2');
-    let flashcardDelete = document.createElement('i');
-
-    flashcardDiv.className = 'flashcard';
-    flashcardDelete.className = 'fa-solid fa-minus';
-
-    question.setAttribute("style", "border-top:1px solid red; padding: 15px; margin-top:30px");
-    question.innerHTML = card.my_question;
-  
-    answer.setAttribute("style", "text-align:center; display:none; color:red; position:absolute; top:100px; left: 40%");
-    answer.innerHTML = card.my_answer;
-    
-    flashcardDiv.append(answer);
-    flashcardDiv.append(question);
-    flashcardDiv.append(flashcardDelete);
-
-    flashcardDiv.addEventListener("click", function(){
-        if(answer.style.display == "none"){
-            answer.style.display = "block";
-        } else {
-            answer.style.display = "none";
-        }
-    });
-
-    flashcardDelete.addEventListener('click', function() {
-        contentArray.splice(currentIndex, 1);
-        localStorage.setItem('items', JSON.stringify(contentArray));
-        flashcardDiv.remove();
-    });
-
-    flashcardsContainer.prepend(flashcardDiv);
+  flashcardsContainer.prepend(flashcardDiv);
 }
 
 //-----------------------------------------------------------
@@ -2022,60 +2047,70 @@ let flag = false; //control the start/stop of the timer;
 
 const timerContainer = document.querySelector('.timer-container');
 
-timerContainer.addEventListener('click', function(){
-    if(!flag) {
-        startTimer();
-        flag = true;
-    } else {
-        stopTimer();
-        flag = false;
-    }
-});
-
-timerContainer.addEventListener('dblclick', function(){
-    resetTimer();
-});
-
-function resetTimer(){
-    clearInterval(startInterval);
-    timeBegan = null;
-    timerStopped = null;
-    stoppedDuration = 0;
-    document.querySelector('.timer-container__display').innerHTML = '00 : 00 : 00';
+timerContainer.addEventListener('click', function () {
+  if (!flag) {
+    startTimer();
+    flag = true;
+  } else {
+    stopTimer();
     flag = false;
+  }
+});
+
+timerContainer.addEventListener('dblclick', function () {
+  resetTimer();
+});
+
+function resetTimer() {
+  clearInterval(startInterval);
+  timeBegan = null;
+  timerStopped = null;
+  stoppedDuration = 0;
+  document.querySelector('.timer-container__display').innerHTML =
+    '00 : 00 : 00';
+  flag = false;
 }
 
 function startTimer() {
-    if(timeBegan === null) timeBegan = new Date();
+  if (timeBegan === null) timeBegan = new Date();
 
-    if(timerStopped !== null) stoppedDuration += (new Date() - timerStopped);
+  if (timerStopped !== null) stoppedDuration += new Date() - timerStopped;
 
-    startInterval = setInterval(clockRunning, 10);
+  startInterval = setInterval(clockRunning, 10);
 }
 
 function stopTimer() {
-    timerStopped = new Date();
-    clearInterval(startInterval);
+  timerStopped = new Date();
+  clearInterval(startInterval);
 }
 
 function clockRunning() {
-    let currentTime = new Date();
-    let timeElapsed = new Date(currentTime - timeBegan - stoppedDuration);
+  let currentTime = new Date();
+  let timeElapsed = new Date(currentTime - timeBegan - stoppedDuration);
 
-    let minutes = timeElapsed.getUTCMinutes();
-    let seconds = timeElapsed.getUTCSeconds();
-    let milliseconds = timeElapsed.getUTCMilliseconds();
+  let minutes = timeElapsed.getUTCMinutes();
+  let seconds = timeElapsed.getUTCSeconds();
+  let milliseconds = timeElapsed.getUTCMilliseconds();
 
-    milliseconds = Math.floor(milliseconds / 10);
+  milliseconds = Math.floor(milliseconds / 10);
 
-    document.querySelector('.timer-container__display').innerHTML =
-    (minutes = minutes < 10 ? '0' + minutes:minutes) + ' : ' +
-    (seconds = seconds < 10 ? '0' + seconds:seconds) + ' : ' +
-    (milliseconds = milliseconds < 10 ? '0' + milliseconds:milliseconds);
+  document.querySelector('.timer-container__display').innerHTML =
+    (minutes = minutes < 10 ? '0' + minutes : minutes) +
+    ' : ' +
+    (seconds = seconds < 10 ? '0' + seconds : seconds) +
+    ' : ' +
+    (milliseconds = milliseconds < 10 ? '0' + milliseconds : milliseconds);
 }
 
 //-----------------------------------------------------------
 
+//Unsplash-API-----------------------------------------------
+
+//-----------------------------------------------------------
+
+//Typing-----------------------------------------------------
+
+//-----------------------------------------------------------
 
 //Check the following:
 
@@ -2113,15 +2148,3 @@ function clockRunning() {
 //
 //
 //
-
-
-
-
-
-
-
-
-
-
-
-
--- js-projects/assets/js/main.js
+++ js-projects/assets/js/main.js
@@ -1,9 +1,11 @@
-//Challenges from codewars
+//Challenges from codewars---------------------------------
 
 // function descendingOrder(n) {
-//     let arr = String(n).split("").map((n) => {
-//         return Number(n);
-//     });
+//     let arr = String(n)
+//         .split("")
+//         .map((n) => {
+//             return Number(n);
+//         });
 //     arr.sort();
 //     arr.reverse();
 //     let str = arr.join("");
@@ -14,26 +16,62 @@
 // let result = descendingOrder(15);
 // console.log(result);
 
-
-// function duplicateCount(text){
-//     let counter = 0;
-//     let secondArr = [""];
-//     let arr = text.toLowerCase().split("");
-//     for(let i = 0; i < arr.length; i++) {
-//         secondFor: for (let j = i+1; j < arr.length; j++) {
-//             if(arr[i] == arr[j]) {
-//                 for (let k = 0; k < secondArr.length; k++) {
-//                     if(secondArr[k] == arr[j]) break secondFor;
-//                     else {
-//                         secondArr.push(arr[j]);
-//                         counter++;
-//                     }    
-//                 }
-//             }
+// function duplicateCount(text) {
+//   let counter = 0;
+//   let secondArr = [""];
+//   let arr = text.toLowerCase().split("");
+//   for (let i = 0; i < arr.length; i++) {
+//     secondFor: for (let j = i + 1; j < arr.length; j++) {
+//       if (arr[i] == arr[j]) {
+//         for (let k = 0; k < secondArr.length; k++) {
+//           if (secondArr[k] == arr[j]) break secondFor;
+//           else {
+//             secondArr.push(arr[j]);
+//             counter++;
+//           }
 //         }
+//       }
 //     }
-//     return counter;
+//   }
+//   return counter;
 // }
 
 // let resultTwo = duplicateCount("ABBA");
 // console.log(resultTwo);
+
+//Server-try--------------------------------------------
+
+// let http = require('http');
+
+// let ourApp = http.createServer(function (req, res) {
+//   res.end('Welcome');
+//   if (req.url == '/something') {
+//     res.end('Here is something on the page!');
+//   }
+// });
+// ourApp.listen(3000);
+
+// let express = require('express');
+// let app = express();
+
+// app.use(express.urlencoded({ extended: false }));
+
+// app.get('/', function (req, res) {
+//   res.send(`
+//   <form action='/answer' method='POST'>
+//     <p>Answer:</p>
+//     <input name="someColor" autocomplete="off">
+//     <button>Submit</button>
+//   </form>
+//   `);
+// }); //(url, func)
+
+// app.post('/answer', function (req, res) {
+//   if (req.body.someColor == 'blue') {
+//     res.send('Thx for sending form!');
+//   } else {
+//     res.send('Nope!');
+//   }
+// });
+
+// app.listen(3000);
